f799d6f30a3c7ee843455fd813cf56b1b05ea0a2
pank <202449477+SeanWortley@users.noreply.github.com>
2026-06-25 11:08:03 +0200

    Bot message on ngrok server

--- bot/src/index.js
+++ bot/src/index.js
@@ -1,20 +1,22 @@
 const express = require("express")
+const { Bot, webhookCallback } = require("grammy")
+
 const PORT = process.env.PORT || 3000
+const app = express()
+const bot = new Bot(process.env.BOT_TOKEN)
 
-const app = express();
-app.use(express.json());
+bot.on("message:text", (ctx) => {
+    ctx.reply(ctx.message.text) // echo
+})
+bot.on("message:text", (ctx) => {
+    console.log("Received:", ctx.message.text)
+    ctx.reply(ctx.message.text)
+})
 
-app.use(async(req, res) => {
-    if (req.method === "POST") {
-        res.send("Hello post");
-    } else if (req.method === "GET") {
-        res.send("Hello get");
-    } else {
-        res.send("Hello");
-    }
-});
+app.use(express.json())
+app.post("/webhook", webhookCallback(bot, "express"))
 
 app.listen(PORT, function(err) {
-    if (err) console.log(err);
-    console.log("Server listening on PORT", PORT);
-});
+    if (err) console.log(err)
+    console.log("Server listening on PORT", PORT)
+})
